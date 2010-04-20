8fef580ac7c601d460bc9680a957244b8ce080c3
Koushik Dutta <koushd@gmail.com>
2010-04-20 00:05:56 -0700

    fix more bugssss

---
+++ kernel/mkbootimg.sh
@@ -0,0 +1,6 @@
+#!/sbin/sh
+echo \#!/sbin/sh > /tmp/createnewboot.sh
+echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base $(cat /tmp/boot.img-base) --output /tmp/newboot.img >> /tmp/createnewboot.sh
+chmod 777 /tmp/createnewboot.sh
+/tmp/createnewboot.sh
+return $?

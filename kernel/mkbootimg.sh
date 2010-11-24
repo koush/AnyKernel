3ba188e5015fd95b34139663ad73fa54bf439584
Brian Crook <brian.w.crook@gmail.com>
2010-11-23 19:15:29 -0500

    Change base address for radio2708

--- kernel/mkbootimg.sh
+++ kernel/mkbootimg.sh
@@ -1,6 +1,6 @@
 #!/sbin/sh
 echo \#!/sbin/sh > /tmp/createnewboot.sh
-echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base 0x19200000 --output /tmp/newboot.img >> /tmp/createnewboot.sh
+echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base 0x02000000 --output /tmp/newboot.img >> /tmp/createnewboot.sh
 chmod 777 /tmp/createnewboot.sh
 /tmp/createnewboot.sh
 return $?

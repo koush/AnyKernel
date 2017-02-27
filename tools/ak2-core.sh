092a3b38809317fc67df68eecdb217a3c72d9aa0
osm0sis <osm0sis@outlook.com>
2017-02-27 16:39:13 -0400

    AK2: add more compression types and Image.fit (ChromeOS) to zImage checks

--- tools/ak2-core.sh
+++ tools/ak2-core.sh
@@ -92,7 +92,7 @@ write_boot() {
     secondoff=`cat *-secondoff`;
     secondoff="--second_offset $secondoff";
   fi;
-  for i in zImage zImage-dtb Image.gz Image.gz-dtb; do
+  for i in zImage zImage-dtb Image.gz Image Image-dtb Image.gz-dtb Image.bz2 Image.bz2-dtb Image.lzo Image.lzo-dtb Image.lzma Image.lzma-dtb Image.xz Image.xz-dtb Image.lz4 Image.lz4-dtb Image.fit; do
     if [ -f /tmp/anykernel/$i ]; then
       kernel=/tmp/anykernel/$i;
       break;

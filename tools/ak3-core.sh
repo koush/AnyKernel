fb5fbb15160c0ebc8696228c259ace1f5d6a250d
osm0sis <osm0sis@outlook.com>
2021-12-15 20:20:27 -0400

    AK3: update KEEPVBMETAFLAG -> PATCHVBMETAFLAG for Canary 23016+

--- tools/ak3-core.sh
+++ tools/ak3-core.sh
@@ -349,19 +349,19 @@ flash_boot() {
             *-dtb) rm -f kernel_dtb;;
           esac;
         fi;
-        unset magisk_patched KEEPFORCEENCRYPT KEEPVERITY SHA1 TWOSTAGEINIT; # leave KEEPVBMETAFLAG set for repack
+        unset magisk_patched KEEPFORCEENCRYPT KEEPVERITY SHA1 TWOSTAGEINIT; # leave PATCHVBMETAFLAG set for repack
       ;;
     esac;
     case $ramdisk_compression in
       none|cpio) nocompflag="-n";;
     esac;
-    case $keep_vbmeta_flag in
-      auto|"") [ "$KEEPVBMETAFLAG" ] || export KEEPVBMETAFLAG=false;;
-      1) export KEEPVBMETAFLAG=true;;
-      *) export KEEPVBMETAFLAG=false;;
+    case $patch_vbmeta_flag in
+      auto|"") [ "$PATCHVBMETAFLAG" ] || export PATCHVBMETAFLAG=false;;
+      1) export PATCHVBMETAFLAG=true;;
+      *) export PATCHVBMETAFLAG=false;;
     esac;
     $bin/magiskboot repack $nocompflag $bootimg $home/boot-new.img;
-    unset KEEPVBMETAFLAG;
+    unset PATCHVBMETAFLAG;
   fi;
   if [ $? != 0 ]; then
     abort "Repacking image failed. Aborting...";

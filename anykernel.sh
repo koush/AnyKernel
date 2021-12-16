d7d7aebf7a516d379de7dde2816384a721caba9c
osm0sis <osm0sis@outlook.com>
2021-12-15 20:29:04 -0400

    anykernel: change keep_vbmeta_flag -> patch_vbmeta_flag

--- anykernel.sh
+++ anykernel.sh
@@ -23,7 +23,7 @@ supported.patchlevels=
 block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-keep_vbmeta_flag=auto;
+patch_vbmeta_flag=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
